2fc20dc913e35061d91e4fd032920105ac563acc
QuantumToast <danieljarski@gmail.com>
2019-03-09 19:27:55 -0600

    Updated Administrator.cs
    Removed unnecessary private fields.

--- Administrator/Administrator.cs
+++ Administrator/Administrator.cs
@@ -13,9 +13,6 @@ namespace Administrator
 {
     public sealed class Administrator
     {
-        private IServiceProvider _provider;
-        private DiscordShardedClient _client;
-
         public async Task InitializeAsync()
         {
             var restClient = new DiscordRestClient();
@@ -23,27 +20,28 @@ namespace Administrator
             
             await restClient.LoginAsync(TokenType.Bot, config.DiscordToken);
             
-            _client = new DiscordShardedClient(new DiscordSocketConfig
+            var client = new DiscordShardedClient(new DiscordSocketConfig
             {
                 MessageCacheSize = 100,
                 LogLevel = LogSeverity.Info,
                 TotalShards = await restClient.GetRecommendedShardCountAsync()
             });
             
-            _provider = ServiceUtilities.AutoBuildServices()
-                .AddSingleton(_client)
+            var provider = ServiceUtilities.AutoBuildServices()
+                .AddSingleton(client)
                 .AddSingleton(restClient)
                 .AddSingleton<CommandService>()
                 .AddSingleton<CancellationTokenSource>()
+                .AddSingleton<Random>()
                 .BuildServiceProvider();
             
-            await ServiceUtilities.InitializeServicesAsync(_provider);
-            await _client.LoginAsync(TokenType.Bot, _provider.GetRequiredService<ConfigurationService>().DiscordToken);
-            await _client.StartAsync();
+            await ServiceUtilities.InitializeServicesAsync(provider);
+            await client.LoginAsync(TokenType.Bot, provider.GetRequiredService<ConfigurationService>().DiscordToken);
+            await client.StartAsync();
 
             try
             {
-                await Task.Delay(-1, _provider.GetRequiredService<CancellationTokenSource>().Token);
+                await Task.Delay(-1, provider.GetRequiredService<CancellationTokenSource>().Token);
             }
             catch (TaskCanceledException)
             { }
