eb3282acea333397390f571844a93baa9358d16b
QuantumToast <danieljarski@gmail.com>
2019-03-12 08:31:36 -0500

    Added command module base

---
+++ Administrator/Commands/AdminModuleBase.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Administrator.Common;
+using Administrator.Services;
+using Discord;
+using Qmmands;
+
+namespace Administrator.Commands
+{
+    public abstract class AdminModuleBase : ModuleBase<AdminCommandContext>, IDisposable
+    {
+        private readonly Stopwatch _watch = Stopwatch.StartNew();
+        
+        public LocalizationService Localization { get; set; }
+
+        protected AdminCommandResult CommandSuccess(string key = null, Embed embed = null, MessageFile file = null,
+            params object[] args)
+            => new AdminCommandResult(_watch.Elapsed, Localization.Localize(Context.Language, key, args), embed, file,
+                true);
+
+        protected AdminCommandResult CommandError(string key, Embed embed = null, MessageFile file = null,
+            params object[] args)
+            => new AdminCommandResult(_watch.Elapsed, Localization.Localize(Context.Language, key, args), embed, file,
+                false);
+
+        protected override async Task BeforeExecutedAsync(Command command)
+        {
+            if (!Context.IsPrivate)
+            {
+                var guild = await Context.Database.GetOrCreateGuildAsync(Context.Guild.Id);
+                Context.SetLanguage(guild.Language);
+                return;
+            }
+
+            var user = await Context.Database.GetOrCreateGlobalUserAsync(Context.User.Id);
+            Context.SetLanguage(user.Language);
+        }
+
+        public void Dispose()
+        {
+            _watch.Stop();
+            Context.Dispose();
+        }
+    }
+}
