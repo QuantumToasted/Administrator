57f462f378e69a304148431336e816976f862d5c
QuantumToast <danieljarski@gmail.com>
2019-03-12 08:55:54 -0500

    Updated AdminCommandContext
    Properties are now assigned in the constructor.

--- Administrator/Commands/AdminCommandContext.cs
+++ Administrator/Commands/AdminCommandContext.cs
@@ -12,25 +12,29 @@ namespace Administrator.Commands
         public AdminCommandContext(DiscordShardedClient client, SocketUserMessage message, string prefix, IServiceProvider provider)
         {
             Client = client;
+            Shard = client.GetShardFor(Guild);
             User = message.Author;
+            Guild = (message.Channel as SocketGuildChannel)?.Guild;
             Message = message;
+            Channel = message.Channel;
+            IsPrivate = message.Channel is IPrivateChannel;
             Prefix = prefix;
             Database = new AdminDatabaseContext(provider);
         }
         
         public DiscordShardedClient Client { get; }
 
-        public DiscordSocketClient Shard => Client.GetShardFor(Guild);
+        public DiscordSocketClient Shard { get; }
         
         public SocketUser User { get; }
 
-        public SocketGuild Guild => (Channel as SocketGuildChannel)?.Guild;
+        public SocketGuild Guild { get; }
         
         public SocketUserMessage Message { get; }
 
-        public ISocketMessageChannel Channel => Message.Channel;
+        public ISocketMessageChannel Channel { get; }
 
-        public bool IsPrivate => Channel is IPrivateChannel;
+        public bool IsPrivate { get; }
         
         public string Prefix { get; }
 
