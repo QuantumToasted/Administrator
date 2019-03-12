9a5790ed92462d143cc50185817f912cc78a489b
QuantumToast <danieljarski@gmail.com>
2019-03-12 08:31:03 -0500

    Added command context

---
+++ Administrator/Commands/AdminCommandContext.cs
@@ -0,0 +1,51 @@
+using System;
+using Administrator.Common;
+using Administrator.Database;
+using Discord;
+using Discord.WebSocket;
+using Qmmands;
+
+namespace Administrator.Commands
+{
+    public sealed class AdminCommandContext : ICommandContext, IDisposable
+    {
+        public AdminCommandContext(DiscordShardedClient client, SocketUserMessage message, string prefix, IServiceProvider provider)
+        {
+            Client = client;
+            User = message.Author;
+            Message = message;
+            Prefix = prefix;
+            Database = new AdminDatabaseContext(provider);
+        }
+        
+        public DiscordShardedClient Client { get; }
+
+        public DiscordSocketClient Shard => Client.GetShardFor(Guild);
+        
+        public SocketUser User { get; }
+
+        public SocketGuild Guild => (Channel as SocketGuildChannel)?.Guild;
+        
+        public SocketUserMessage Message { get; }
+
+        public ISocketMessageChannel Channel => Message.Channel;
+
+        public bool IsPrivate => Channel is IPrivateChannel;
+        
+        public string Prefix { get; }
+
+        public AdminDatabaseContext Database { get; }
+        
+        public LocalizedLanguage Language { get; private set; }
+
+        public void SetLanguage(LocalizedLanguage language)
+        {
+            Language = language;
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
