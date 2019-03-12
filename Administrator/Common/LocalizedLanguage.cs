797aa2a96ccede41401868cda9e2f0929c953484
QuantumToast <danieljarski@gmail.com>
2019-03-12 08:23:13 -0500

    Added LocalizedLanguage + example files
    will be used for localization of commands and other things

---
+++ Administrator/Common/LocalizedLanguage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace Administrator.Common
+{
+    public sealed class LocalizedLanguage
+    {
+        [JsonProperty("code")]
+        public string CultureCode { get; private set; }
+
+        [JsonProperty("englishName")]
+        public string EnglishName { get; private set; }
+
+        [JsonProperty("nativeName")]
+        public string NativeName { get; private set; }
+
+        [JsonProperty("responses")]
+        public IReadOnlyDictionary<string, ImmutableArray<string>> Responses { get; private set; }
+
+        public string Localize(string str, params object[] args)
+        {
+            try
+            {
+                return string.Format(new CultureInfo(CultureCode), str, args);
+            }
+            catch (CultureNotFoundException)
+            {
+                return string.Format(str, args);
+            }
+        }
+    }
+}
