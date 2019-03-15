7bea269775ec57bdd474520156a00e9bffab9dcd
Daniel Jarski <danieljarski@gmail.com>
2019-03-14 22:41:14 -0500

    Added embed extensions
    For success color, warning color, and error color embeds

---
+++ Administrator/Extensions/EmbedExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Administrator.Services;
+using Discord;
+
+namespace Administrator.Extensions
+{
+    public static class EmbedExtensions
+    {
+        private static readonly ConfigurationService Config = ConfigurationService.Basic;
+
+        public static EmbedBuilder WithSuccessColor(this EmbedBuilder builder)
+            => builder.WithColor(Config.SuccessColor);
+
+        public static EmbedBuilder WithWarnColor(this EmbedBuilder builder)
+            => builder.WithColor(Config.WarnColor);
+
+        public static EmbedBuilder WithErrorColor(this EmbedBuilder builder)
+            => builder.WithColor(Config.ErrorColor);
+    }
+}
