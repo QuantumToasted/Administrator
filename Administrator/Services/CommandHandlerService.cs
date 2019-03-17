f558af189fa1fba280474b8a546b94bf133d4e9f
Daniel Jarski <danieljarski@gmail.com>
2019-03-16 23:33:41 -0500

    Updated CommandHandlerService
    outsource the error embed building to a method and cleaned it up a bit to make it pretty pretty.

--- Administrator/Services/CommandHandlerService.cs
+++ Administrator/Services/CommandHandlerService.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -63,33 +63,12 @@ namespace Administrator.Services
             var result = await _commands.ExecuteAsync(input, context, _provider);
 
             if (!(result is FailedResult failedResult)) return true;
-            
-            // TODO: localized error messages, log 
-            var error = new StringBuilder()
-                .Append(failedResult switch
-            {
-                ParameterChecksFailedResult parameterChecksResult => string.Join('\n', parameterChecksResult.FailedChecks.Select(x => x.Error)),
-                ChecksFailedResult checkResult => string.Join('\n', checkResult.FailedChecks.Select(x => x.Error)),
-                ExecutionFailedResult execResult => GenerateException(execResult.Exception),
-                CommandNotFoundResult _ => string.Empty,
-                _ => failedResult.Reason
-            }).ToString();
 
-            if (!string.IsNullOrWhiteSpace(error))
-                await context.Channel.SendMessageAsync(embed: new EmbedBuilder().WithErrorColor()
-                    .WithTitle(context.Localize("commanderror")).WithDescription(error).Build());
-            
+            var errorEmbed = await BuildErrorEmbedAsync(context, failedResult);
+            if (!(errorEmbed is null))
+                await context.Channel.SendMessageAsync(embed: errorEmbed);
+
             return false;
-
-            string GenerateException(Exception ex)
-            {
-                _logging.LogErrorAsync(ex, "CommandHandler");
-                var frames = new StackTrace(ex, true).GetFrames();
-                var frame = frames.First(x => x.GetFileName()?.Contains("Administrator") == true);
-                var message = Convert.ToBase64String(Encoding.UTF8.GetBytes(
-                    $"{ex.Message} - at {frame.GetFileName()}, line {frame.GetFileLineNumber()} - {DateTimeOffset.UtcNow:g} UTC"));
-                return context.Localize("commanderror_exception", ConfigurationService.SUPPORT_GUILD, message);
-            }
         }
 
         public async Task SendCommandResultAsync(Command command, AdminCommandResult result,
@@ -107,6 +86,66 @@ namespace Administrator.Services
             {
                 await context.Channel.SendMessageAsync(result.Text ?? string.Empty, embed: result.Embed);
             }
+        }
+
+        private async Task<Embed> BuildErrorEmbedAsync(AdminCommandContext context, FailedResult failedResult)
+        {
+            var builder = new StringBuilder();
+            switch (failedResult)
+            {
+                case ArgumentParseFailedResult argumentParseResult:
+                    if (argumentParseResult.Position.HasValue)
+                    {
+                        // TODO: Replace with context.Path on next Qmmands update
+                        var center = context.Prefix.Length + argumentParseResult.Command.FullAliases[0].Length + argumentParseResult.Position.Value;
+                        var fullString = $"{context.Prefix}{argumentParseResult.Command.FullAliases[0]} {argumentParseResult.RawArguments}"
+                            .FixateTo(ref center, 30 - (context.Prefix.Length + argumentParseResult.Command.FullAliases[0].Length));
+                        builder.AppendLine(Format.Code($"{fullString}\n{"↑".PadLeft(center + 2)}"));
+                    }
+                    builder.AppendLine(argumentParseResult.ArgumentParserFailure switch
+                        {
+                            ArgumentParserFailure.TooFewArguments => context.Localize("commanderror_toofewarguments",
+                                argumentParseResult.Command.Parameters.Count),
+                            ArgumentParserFailure.TooManyArguments => context.Localize("commanderror_toomanyarguments",
+                                argumentParseResult.Command.Parameters.Count),
+                            // TODO: Localize the rest of the errors.
+                            _ => argumentParseResult.Reason
+                        });
+                    break;
+                case ChecksFailedResult checkResult:
+                    builder.AppendLine(context.Localize("commanderror_checks",
+                        string.Join('\n', checkResult.FailedChecks.Select(x => x.Error))));
+                    break;
+                case CommandNotFoundResult _:
+                case CommandOnCooldownResult _:
+                    return null;
+                case ExecutionFailedResult execResult:
+                    await _logging.LogErrorAsync(execResult.Exception, "CommandHandler");
+                    var frames = new StackTrace(execResult.Exception, true).GetFrames();
+                    var frame = frames.First(x => x.GetFileName()?.Contains("Administrator") == true);
+                    var message = Convert.ToBase64String(Encoding.UTF8.GetBytes(
+                        $"{execResult.Exception.Message} - at {frame.GetFileName()}, line {frame.GetFileLineNumber()} - {DateTimeOffset.UtcNow:g} UTC"));
+                    builder.AppendLine(context.Localize("commanderror_exception",
+                        ConfigurationService.SUPPORT_GUILD, Format.Code(message, "cs")));
+                    break;
+                case OverloadsFailedResult overloadResult:
+                    return await BuildErrorEmbedAsync(context,
+                        overloadResult.FailedOverloads.Values.FirstOrDefault(x => !(x is ArgumentParseFailedResult)) ??
+                        overloadResult.FailedOverloads.Values.First());
+                case ParameterChecksFailedResult paramCheckResult:
+                    builder.AppendLine(context.Localize("commanderror_paramchecks",
+                        string.Join('\n', paramCheckResult.FailedChecks.Select(x => x.Error))));
+                    break;
+                case TypeParseFailedResult typeParseResult:
+                    builder.AppendLine(context.Localize("commanderror_typeparsefailed",
+                        Format.Code(typeParseResult.Parameter.Name), typeParseResult.Reason));
+                    break;
+            }
+
+            var value = builder.ToString();
+            return !string.IsNullOrWhiteSpace(value)
+                ? new EmbedBuilder().WithErrorColor().WithTitle(context.Localize("commanderror")).WithDescription(value).Build()
+                : null;
         }
 
         Task IService.InitializeAsync()
