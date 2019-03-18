faf75cbd7ee22d74d3da85c4db99ebbe863d69c6
Daniel Jarski <danieljarski@gmail.com>
2019-03-18 16:09:24 -0500

    Reworked Softban -> TemporaryBan
    Softbans have been renamed to TemporaryBans, and have a duration.

--- Administrator/Database/Models/Punishments/Softban.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Administrator.Database
-{
-    public sealed class Softban : Punishment
-    {
-        public Softban(ulong guildId, ulong targetId, ulong moderatorId, string reason) 
-            : base(guildId, targetId, moderatorId, reason)
-        { }
-    }
-}
