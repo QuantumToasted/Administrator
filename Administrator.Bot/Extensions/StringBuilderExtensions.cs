aefd312aba815210d3f6977f3a17a23e44cf8978
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2025-01-05 16:19:42 -0600

    Fix AppendJoinTruncated working incorrectly...
    ...in certain circumstances - with very specific string lengths, the final StringBuilder returned could be a few characters longer than `length`.

--- Administrator.Bot/Extensions/StringBuilderExtensions.cs
+++ Administrator.Bot/Extensions/StringBuilderExtensions.cs
@@ -8,18 +8,38 @@ public static class StringBuilderExtensions
         => sb.Append($"{text}\n");
 
     public static StringBuilder AppendJoinTruncated<T>(this StringBuilder sb, string separator, IEnumerable<T> values, int length, Func<int, string>? remainderFormatter = null)
+        where T : notnull
     {
         remainderFormatter ??= static i => $"{i} more...";
 
         var list = values.ToList();
+        string? nextFormatted = null;
         for (var i = 0; i < list.Count; i++)
         {
-            var formatted = list[i]!.ToString();
+            var formatted = nextFormatted ?? list[i].ToString();
+            
+            // let's assume that the first, single element will not exceed 'length'...
+            sb.Append(formatted);
+
+            if (i > 0 && i < list.Count - 1)
+                sb.Append(separator);
+
+            if (i == list.Count - 1)
+                continue;
+            
+            nextFormatted = list[i + 1].ToString();
+            var remainderLine = remainderFormatter(list.Count - i - 1);
+
+            if (sb.Length + nextFormatted!.Length + separator.Length + remainderLine.Length >= length)
+            {
+                return sb.Append(remainderLine);
+            }
 
             // subtract an extra position:
             // if length is too long we need to ignore the current value of i
-            var remainderLine = remainderFormatter(list.Count - i - 1);
+            //var remainderLine = remainderFormatter(list.Count - i - 1);
 
+            /*
             if (sb.Length + formatted!.Length + separator.Length + remainderLine.Length >= length)
             {
                 return sb.Append(separator).Append(remainderLine);
@@ -29,6 +49,7 @@ public static class StringBuilderExtensions
                 sb.Append(separator);
 
             sb.Append(formatted);
+            */
         }
 
         return sb;
