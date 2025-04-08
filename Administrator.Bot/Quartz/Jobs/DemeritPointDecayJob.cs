1d9c01ed5f2c928e26236accd209d90df58c3d5c
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2025-04-07 21:55:26 -0500

    Cleanup and revamp demerit point decay
    This should actually fix it.

--- Administrator.Bot/Quartz/Jobs/DemeritPointDecayJob.cs
+++ Administrator.Bot/Quartz/Jobs/DemeritPointDecayJob.cs
@@ -7,97 +7,71 @@ namespace Administrator.Bot.Jobs;
 
 public sealed class DemeritPointDecayJob(ILogger<DemeritPointDecayJob> logger, AdminDbContext db) : IAdminJob<DemeritPointDecayJob, Warning>
 {
-    private int _totalDemeritPoints = -1;
-    
     public ILogger Logger { get; } = logger;
     
     public async ValueTask<Warning> GetEntity(IJobExecutionContext context, int warningId)
     {
         var warning = await db.Punishments.OfType<Warning>().FirstAsync(x => x.Id == warningId);
-        _totalDemeritPoints = await db.Punishments.GetCurrentDemeritPointsAsync(warning.GuildId, warning.Target.Id);
         return warning;
     }
 
+    // we are ONLY decaying demerit points for the warning. Let re-scheduling determine the next decay.
     public async ValueTask Execute(IJobExecutionContext context, Warning warning)
     {
-        var guild = await db.Guilds.GetOrCreateAsync(warning.GuildId);
-        
-        if (guild.DemeritPointsDecayInterval is not { } interval)
-        {
-            Logger.LogWarning("Guild {GuildId} does not have a demerit point decay interval, but the decay job fired anyways.", warning.GuildId.RawValue);
-            return;
-        }
-
-        if (warning.DemeritPointsRemaining == 0)
-        {
-            Logger.LogWarning("Warning {Id} for member [{GuildId},{UserId}] DemeritPointsRemaining == 0 - was likely revoked.", warning.Id, warning.GuildId.RawValue, warning.Target.Id);
-            _totalDemeritPoints = await db.Punishments.GetCurrentDemeritPointsAsync(warning.GuildId, warning.Target.Id);
-        }
-        else
-        {
-            warning.DemeritPointsRemaining -= 1; // could use --
-            _totalDemeritPoints -= 1;
-        }
-        
-        var member = await db.Members.GetOrCreateAsync(warning.GuildId, warning.Target.Id);
-
-        if (_totalDemeritPoints > 0) 
-        {
-            member.NextDemeritPointDecay = (member.NextDemeritPointDecay ?? DateTimeOffset.UtcNow) + interval;
-        }
-        else // 1 -> 0 total points (final warning)
-        {
-            member.NextDemeritPointDecay = null;
-        }
-
+        warning.DemeritPointsRemaining = Math.Max(0, warning.DemeritPointsRemaining - 1);
         await db.SaveChangesAsync();
-
-        var currentPoints = await db.Punishments.GetCurrentDemeritPointsAsync(warning.GuildId, warning.Target.Id);
-        Logger.LogDebug("Member [{GuildId},{UserId}] has decayed to {Points} demerit points. Next decay: {NextDecay}",
-            warning.GuildId.RawValue, warning.Target.Id, currentPoints, member.NextDemeritPointDecay);
     }
 
     public async ValueTask Reschedule(IJobExecutionContext context, Warning warning, CancellationToken cancellationToken)
     {
-        if (_totalDemeritPoints == -1)
+        Warning? nextWarning = null;
+        if (warning.DemeritPointsRemaining == 0)
         {
-            Logger.LogError("_totalDemeritPoints was -1! State was not preserved.");
-            return;
-        }
-
-        if (_totalDemeritPoints == 0) // 1 -> 0 total points (final warning)
-        {
-            Logger.LogDebug("_totalDemeritPoints was 0. Not rescheduling due to no more points to decay.");
-            return;
+            // get the next available warning that can decay
+            nextWarning = await db.Punishments.OfType<Warning>()
+                .Where(x => x.GuildId == warning.GuildId && x.Target.Id == warning.Target.Id)
+                .Where(x => x.Id != warning.Id && x.RevokedAt == null && x.DemeritPointsRemaining > 0)
+                .OrderByDescending(x => x.Id)
+                .FirstOrDefaultAsync(cancellationToken);
         }
         
         var member = await db.Members.GetOrCreateAsync(warning.GuildId, warning.Target.Id);
-        if (!member.NextDemeritPointDecay.HasValue)
+        if (nextWarning is null && warning.DemeritPointsRemaining == 0)
         {
-            Logger.LogWarning("Member [{GuildId},{UserId}] does not have a next decay time, even though they should.", warning.GuildId.RawValue, warning.Target.Id);
+            Logger.LogDebug("Member [{GuildId},{UserId}] has reached 0 demerit points. Not rescheduling warning decay.", warning.GuildId.RawValue, warning.Target.Id);
+
+            member.NextDemeritPointDecay = null;
+            await db.SaveChangesAsync(cancellationToken);
             return;
         }
 
-        if (warning.DemeritPointsRemaining == 0)
-        {
-            // schedule for the next warning
-            var nextWarning = await db.Punishments.OfType<Warning>()
-                .OrderByDescending(x => x.Id)
-                .Where(x => x.DemeritPointsRemaining > 0 && x.RevokedAt == null && x.Target.Id == warning.Target.Id && x.GuildId == warning.GuildId)
-                .FirstOrDefaultAsync(cancellationToken);
+        var guild = await db.Guilds.GetOrCreateAsync(warning.GuildId);
 
-            if (nextWarning is null)
-            {
-                Logger.LogDebug("No warnings left to decay for member [{GuildId},{UserId}], not scheduling a decay job.", warning.GuildId.RawValue, warning.Target.Id);
-            }
-            else
-            {
-                await context.Scheduler.ScheduleDemeritPointExpiryJob(nextWarning, member, cancellationToken);
-            }
+        if (guild.DemeritPointsDecayInterval is not { } decayInterval)
+        {
+            Logger.LogWarning("Guild [{GuildId}] has removed their decay interval. Not rescheduling warning decay.", warning.GuildId.RawValue);
+            return; // Don't set their decay to null
+        }
+        
+        var nextDecay = member.NextDemeritPointDecay + decayInterval;
+        if (nextDecay < nextWarning?.CreatedAt)
+        {
+            // Note to self: this is an exceptional case that may only result if I am in the process of fixing broken decay.
+            nextDecay = nextWarning.CreatedAt + decayInterval;
             
-            return;
+            Logger.LogDebug("Found a warning newer than member [{GuildId},{UserId}]'s next decay. Bumping it to {StartAt}.",
+                nextWarning.GuildId.RawValue, nextWarning.Target.Id, nextDecay);
         }
+        
+        // TODO: this will be set AFTER any modifications
+        member.NextDemeritPointDecay = nextDecay;
+        await db.SaveChangesAsync(cancellationToken);
+        
+        await context.Scheduler.RescheduleDemeritPointExpiryJob(context.Trigger.Key, nextWarning ?? warning, member, cancellationToken);
+    }
 
-        await context.Scheduler.RescheduleDemeritPointExpiryJob(context.Trigger.Key, warning, member, cancellationToken);
+    public static JobKey FormatJobKey(Warning entity)
+    {
+        return JobKey.Create($"{entity.GuildId}:{entity.Target.Id}", nameof(Warning));
     }
 }
